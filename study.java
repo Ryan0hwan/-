01ac327eb08ef1c5f6ea00c1b4cea6859768523f
Younghwan <158720833+Ryan0hwan@users.noreply.github.com>
2024-05-03 02:21:37 +0900

    refactor : 자바 100제 모음
    볼만한것만 몇개

---
+++ study.java
@@ -0,0 +1,47 @@
+# 정수형 1개 선언, 변수에 저장, 변수 출력
+import java.io.*;
+
+public class Main{
+  public static void main(String []args)throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int a = Integer.parseInt(br.readLine());
+    System.out.print(a);
+  }
+}
+
+  
+# 정수형 2개 선언, 변수에 저장, 변수 출력
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        System.out.print(a + " " + b);
+    }
+}
+
+
+# 연.월.일. 입력받아, 연,월,일을 yyyy.mm.dd형식으로 출력
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String arr[] = a.split("\\.");
+
+        int A = Integer.parseInt(arr[0]);
+        int B = Integer.parseInt(arr[1]);
+        int C = Integer.parseInt(arr[2]);
+
+        System.out.format("%04d.%02d.%02d", A, B, C);
+    }
+}
