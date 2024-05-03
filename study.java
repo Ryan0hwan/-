51f8d3ba14f4f1c7223c1698eb5b8355aca11b20
Younghwan <158720833+Ryan0hwan@users.noreply.github.com>
2024-05-03 18:45:00 +0900

    refactor : 몇개추가

--- study.java
+++ study.java
@@ -28,7 +28,6 @@ public class Main {
 
 
 # 연.월.일. 입력받아, 연,월,일을 yyyy.mm.dd형식으로 출력
-
 import java.io.*;
 
 public class Main {
@@ -45,3 +44,19 @@ public class Main {
         System.out.format("%04d.%02d.%02d", A, B, C);
     }
 }
+
+# 1025. 정수 1개 입력받고, 나누어 출력하기. 
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String arr[] = br.readLine().split("");
+        
+        for(int i = 0; i <5 ; i++){
+            int k = Integer.parseInt(arr[i]);
+            System.out.println("[" +(int)(k*Math.pow(10,4-i))+ "]");
+        }
+    }
+}
+
